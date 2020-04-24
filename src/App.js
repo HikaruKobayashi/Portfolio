a7d12a6b600cc5848175c78a1816b31b465eabb3
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-04-24 19:40:53 +0900

    Portfolioとしての要素を揃えた。

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const style = {
+const stylee = {
   width: '100%',
-  height: '11rem',
-  backgroundImage: `url(${process.env.PUBLIC_URL}/swim.jpg)`
+  height: '40vmin',
+  backgroundImage: `url(${process.env.PUBLIC_URL}/starry-sky-2.jpg)`
+};
+
+const styleee = {
+  width: '100%',
+  height: '60vmin',
+  backgroundImage: `url(${process.env.PUBLIC_URL}/sky.jpg)`
 };
 
 function App() {
@@ -13,73 +19,80 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="scroll-bg bg-color01">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2 className="sub-title">Hikaru Kobayashi</h2>
-          <h3>Social Networking Service</h3>
-            <p>Twitter</p>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2 className="sub-title">Hikaru Kobayashi</h2>
+        </div>
+
+        <div className="fixed-bg bg01" style={ styleee }></div>
+
+        <div className="scroll-bg bg-color01">
+          <h2 className="sub-title">Who am I?</h2>
           <h3>History</h3>
-            <p>1982         長野県中野市に生まれる。</p>
-            <p>2005-2011    延徳小学校</p>
-            <p>2011-2014    南宮中学校</p>
-            <p>2014-2017    長野日本大学高等学校</p>
-            <p>2017-current 日本大学 経済学部</p>
+            <p>1982         I was born in Nakano City, Nagano Prefecture.</p>
+            <p>2005-2011    Entoku Elementary School</p>
+            <p>2011-2014    Nangu Junior High School</p>
+            <p>2014-2017    Nagano Nihon University High School</p>
+            <p>2017-current Nihon University Faculty of Economics</p>
           <h3>Social Networking Service</h3>
             <p>Twitter</p>
             <p>Instagram</p>
             <p>Git Hub</p>
         </div>
 
-        <div className="fixed-bg bg01" style={ style }>
-          <p>コンテンツ</p>
-        </div>
-
         <div className="scroll-bg bg-color01">
           <h2 className="sub-title">Swimming</h2>
-          <img src={`${process.env.PUBLIC_URL}/swim.jpg`} className="swim-logo" />
-        </div>
-
-        <div className="fixed-bg bg01" style={ style }>
-          <p>コンテンツ</p>
+          <div className="swim-punctuation-left">
+            <h3>Discipline</h3>
+              <p>Breaststroke</p>
+              <p>100m 1:09</p>
+              <p>200m 2:24</p>
+              <p>Individual medley</p>
+              <p>200m 2:10</p>
+              <p>400m 4:44</p>
+          </div>
+          <div className="swim-punctuation-right">
+            <img src={`${process.env.PUBLIC_URL}/swim.jpg`} className="swim-logo" />
+          </div>
         </div>
 
         <div className="scroll-bg bg-color01">
           <h2 className="sub-title">Skill</h2>
-          <div>
-            <div className="punctuation">
-              <img src={`${process.env.PUBLIC_URL}/ubuntu.png`} className="docker-logo" />
-            </div>
-            <div className="punctuation">
-              <img src={`${process.env.PUBLIC_URL}/html.png`} className="docker-logo" />
-            </div>
-            <div className="punctuation">
-              <img src={`${process.env.PUBLIC_URL}/docker-logo.png`} className="docker-logo" />
-            </div>
-            <div className="punctuation">
-              <img src={`${process.env.PUBLIC_URL}/slack.svg`} className="docker-logo" />
-            </div>
-            <div className="punctuation">
-            <img src={`${process.env.PUBLIC_URL}/Octocat.png`} className="git-hub-logo" />
-            </div>
-            <div className="punctuation">
-              <img src={`${process.env.PUBLIC_URL}/aws.png`} className="docker-logo" />
-            </div>
-            <div className="punctuation">
-              <img src={logo} className="docker-logo" />
-            </div>
-            <div className="punctuation">
-              <img src={`${process.env.PUBLIC_URL}/rails.png`} className="docker-logo" />
-            </div>
+          <div className="punctuation-left">
+            <img src={`${process.env.PUBLIC_URL}/html.png`} className="skill-logo" />
+            <h3 className="skill-title">HTML・CSS</h3>
+          </div>
+          <div className="punctuation-right">
+            <img src={`${process.env.PUBLIC_URL}/docker-logo.png`} className="skill-logo" />
+            <h3 className="skill-title">Docker</h3>
+          </div>
+          <div className="punctuation-left">
+            <img src={`${process.env.PUBLIC_URL}/aws.png`} className="skill-logo" />
+            <h3 className="skill-title">AWS</h3>
+          </div>
+          <div className="punctuation-right">
+            <img src={logo} className="skill-logo" />
+            <h3 className="skill-title">React</h3>
+          </div>
+          <div className="punctuation-left">
+            <img src={`${process.env.PUBLIC_URL}/rails.png`} className="skill-logo" />
+            <h3 className="skill-title">Ruby on Rails</h3>
+          </div>
+          <div className="punctuation-right">
+            <img src={`${process.env.PUBLIC_URL}/ubuntu.png`} className="skill-logo" />
+            <h3 className="skill-title">Ubuntu</h3>
+          </div>
+          <div className="punctuation-left">
+            <img src={`${process.env.PUBLIC_URL}/slack.svg`} className="skill-logo" />
+            <h3 className="skill-title">Slack</h3>
+          </div>
+          <div className="punctuation-right">
+            <img src={`${process.env.PUBLIC_URL}/Octocat.png`} className="skill-logo" />
+            <h3 className="skill-title">Git Hub</h3>
           </div>
         </div>
 
-        <div className="fixed-bg bg01" style={ style }>
-          <p>コンテンツ</p>
-        </div>
-
         <div className="scroll-bg bg-color01">
-          <h2 className="sub-title">Work</h2>
-          <h3>Blog</h3>
-          <h3>Money Hub</h3>
+          <h2 className="sub-title">Thank you for reading.</h2>
         </div>
       </header>
     </div>
